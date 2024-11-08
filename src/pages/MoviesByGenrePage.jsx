7f665c0c4c7fe59a143d33abc2126cd9ec58944a
diegoc85 <diegoc.spv85@gmail.com>
2024-11-08 20:35:56 -0300

    alteração MoviesGenre

--- src/pages/MoviesByGenrePage.jsx
+++ src/pages/MoviesByGenrePage.jsx
@@ -2,7 +2,7 @@ export default function MoviesByGenrePage(){
     return(
         <>
         <h1>Movies By Genre Page</h1>
-        {/*Lista todos os filmes do gênero selecionado pelo usuário. */}
+        {/*Listagem dos filmes do gênero selecionado pelo usuário. */}
         </>
     )
 }
